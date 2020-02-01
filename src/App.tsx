e63fa7ac3283a3644a8fe6234aef2a3acebaf0d2
Stephen Davies <steve9164@gmail.com>
2020-02-02 01:49:35 +1100

    Add material-ui, file uploading, image history

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import React, { useState, useRef } from "react";
-import pako from "pako";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  makeStyles,
+  TextField,
+  Typography
+} from "@material-ui/core";
 import { saveAs } from "file-saver";
-import "./App.css";
+import { DropzoneArea } from "material-ui-dropzone";
+import { observable } from "mobx";
+import { useObserver } from "mobx-react-lite";
+import pako from "pako";
+import React, { useRef, useState } from "react";
 
 const QUOKKA_OLED_WIDTH = 128;
 const QUOKKA_OLED_HEIGHT = 64;
-
-function getScaledDimensions(sourceImage: HTMLImageElement) {
-  // Fix this...
-  const downScale =
-    sourceImage.width > QUOKKA_OLED_WIDTH ||
-    sourceImage.height > QUOKKA_OLED_HEIGHT;
-  const scaledWidth = downScale ? QUOKKA_OLED_WIDTH : sourceImage.width;
-  const scaledHeight = downScale ? QUOKKA_OLED_HEIGHT : sourceImage.height;
-  return { scaledWidth, scaledHeight };
-}
 
 function isWhitePixel(rgba: Uint8Array | Uint8ClampedArray | number[]) {
   return 0.299 * rgba[0] + 0.587 * rgba[1] + 0.114 * rgba[2] > 127;
 }
 
-function downloadQimz(
-  sourceImage: HTMLImageElement,
-  destCanvas: HTMLCanvasElement
-) {
-  // Update the canvas before downloading
-  blitImage(sourceImage, destCanvas);
-  const { scaledWidth, scaledHeight } = getScaledDimensions(sourceImage);
-  const ctx = destCanvas.getContext("2d")!;
-  const imageData = ctx.getImageData(0, 0, scaledWidth, scaledHeight);
+function processImage(
+  source: HTMLImageElement,
+  dest: HTMLCanvasElement
+): Uint8Array {
+  const scaleFactor = Math.min(
+    QUOKKA_OLED_WIDTH / source.width,
+    QUOKKA_OLED_HEIGHT / source.height,
+    1
+  );
+  const width = scaleFactor * source.width;
+  const height = scaleFactor * source.height;
+  dest.width = width;
+  dest.height = height;
+
+  const ctx = dest.getContext("2d")!;
+  ctx.clearRect(0, 0, width, height);
+  ctx.drawImage(source, 0, 0, width, height);
+  const imageData = ctx.getImageData(0, 0, width, height);
   const data = imageData.data;
+  // Iterate and draw to canvas
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const monochrome = isWhitePixel(data.slice(i, i + 4)) ? 255 : 0;
+    data[i] = monochrome;
+    data[i + 1] = monochrome;
+    data[i + 2] = monochrome;
+  }
+  ctx.putImageData(imageData, 0, 0);
+  // Iterate again, saving to qimz
   const pixelData = new Uint8Array(Math.ceil(imageData.data.length / 32.0));
   // Iterate over the image data turning each 8 pixels into a byte
   for (let i = 0; i < imageData.data.length; i += 32) {
@@ -45,79 +67,184 @@ function downloadQimz(
   // Compress pixels and construct qimz
   const compressed: Uint8Array = pako.deflate(pixelData);
   const qimz = new Uint8Array(compressed.length + 2);
-  qimz.set([scaledWidth, scaledHeight]);
+  qimz.set([width, height]);
   qimz.set(compressed, 2);
-
-  // Create a blob and download as a file
-  const blob = new Blob([qimz], { type: "application/octet-stream" });
-  saveAs(blob, "image.qimz");
+  return qimz;
 }
 
-function blitImage(
-  sourceImage: HTMLImageElement,
-  destCanvas: HTMLCanvasElement
-) {
-  const { scaledWidth, scaledHeight } = getScaledDimensions(sourceImage);
-  const ctx = destCanvas.getContext("2d")!;
-  ctx.drawImage(sourceImage, 0, 0, scaledWidth, scaledHeight);
-  const imageData = ctx.getImageData(0, 0, scaledWidth, scaledHeight);
-  const data = imageData.data;
-  for (let i = 0; i < imageData.data.length; i += 4) {
-    const monochrome = isWhitePixel(data.slice(i, i + 4)) ? 255 : 0;
-    data[i] = monochrome;
-    data[i + 1] = monochrome;
-    data[i + 2] = monochrome;
+const useImagePreviewStyles = makeStyles({
+  image: {
+    maxHeight: 512,
+    maxWidth: 512
+  },
+  previewBorder: {
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "black",
+    width: 128,
+    height: 64
   }
-  ctx.putImageData(imageData, 0, 0);
+});
+
+interface ImagePreviewProps {
+  url: string;
 }
 
-const App: React.FC = () => {
+const ImagePreview: React.FC<ImagePreviewProps> = ({ url }) => {
   const imageEl = useRef<HTMLImageElement>(null);
   const monochromeCanvasEl = useRef<HTMLCanvasElement>(null);
-  const [url, setUrl] = useState(
-    "https://groklearning-cdn.com/static/images/challenge/ncss-logo-fb.png"
-  );
+  const [qimz, setQimz] = useState();
+  const [temporaryUrl, setTemporaryUrl] = useState("");
+  const classes = useImagePreviewStyles();
   return (
-    <div className="App">
+    <>
+      <TextField
+        variant="filled"
+        label="Image URL"
+        value={temporaryUrl}
+        onChange={evt => setTemporaryUrl(evt.target.value)}
+      />
+      <Button variant="contained" size="large">
+        Load image
+      </Button>
+
       <div>
-        <img alt="Source" ref={imageEl} src={url} crossOrigin="anonymous" />
-        <canvas ref={monochromeCanvasEl} />
+        <img
+          className={classes.image}
+          alt="Source"
+          ref={imageEl}
+          src={url}
+          crossOrigin="anonymous"
+          onLoad={() =>
+            setQimz(processImage(imageEl.current!, monochromeCanvasEl.current!))
+          }
+        />
+        <div className={classes.previewBorder}>
+          <canvas ref={monochromeCanvasEl} />
+        </div>
+        <button
+          onClick={() => {
+            // Create a blob and download as a file
+            const blob = new Blob([qimz], { type: "application/octet-stream" });
+            saveAs(blob, "image.qimz");
+          }}
+        >
+          Download Qimz
+        </button>
       </div>
-      <form>
+    </>
+  );
+};
+
+const useStylesHistoryImage = makeStyles({
+  // card: { maxWidth: 200 },
+  media: { height: 160 }
+});
+
+interface HistoryImageProps {
+  url: string;
+  caption?: string;
+  onClick?: () => void;
+}
+
+const HistoryImage: React.FC<HistoryImageProps> = ({
+  url,
+  caption,
+  onClick
+}) => {
+  const classes = useStylesHistoryImage();
+  return (
+    <Card>
+      <CardActionArea onClick={onClick}>
+        <CardMedia className={classes.media} image={url} />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {caption || url}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+interface IDraggedImage {
+  readonly data: string;
+  readonly name: string;
+}
+
+const DEFAULT_URL =
+  "http://ci.terria.io/master/build/78303112301085744794e356ff2dca25.png";
+
+const createStore = () => ({
+  urls: observable.box([DEFAULT_URL]),
+  addUrl(url: string) {
+    this.urls.set([...this.urls.get(), url]);
+  },
+  removeUrl(index: number) {
+    const cloned = this.urls.get().slice();
+    cloned.splice(index, 1);
+    this.urls.set(cloned);
+  },
+  imageUrl: observable.box(DEFAULT_URL),
+  files: observable.box([] as IDraggedImage[]),
+  addLocalImage(file: File) {
+    const reader = new FileReader();
+    const name = file.name;
+    reader.onerror = () => console.log("file reading has failed");
+    reader.onload = e => {
+      const data = reader.result! as string;
+      this.files.set([...this.files.get(), { name, data }]);
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+const App: React.FC = () => {
+  const [store, _] = useState(createStore());
+  const [url, setUrl] = useState("");
+
+  return useObserver(() => (
+    <Container>
+      <Typography variant="h2">Quokka Image Converter</Typography>
+      <ImagePreview url={store.imageUrl.get()} />
+      <Grid container direction="row" justify="center" spacing={2}>
+        {store.urls.get().map(url => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={url}>
+            <HistoryImage url={url} onClick={() => store.imageUrl.set(url)} />
+          </Grid>
+        ))}
+        {store.files.get().map(({ name, data }) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={data}>
+            <HistoryImage
+              key={data}
+              caption={name}
+              url={data}
+              onClick={() => store.imageUrl.set(data)}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <DropzoneArea
+        maxFileSize={20 * 1e6}
+        acceptedFiles={["image/*"]}
+        onDrop={file => store.addLocalImage(file)}
+      />
+      <form
+        noValidate
+        onSubmit={evt => {
+          store.addUrl(url);
+          evt.preventDefault();
+        }}
+      >
         <input
           type="text"
           value={url}
           onChange={evt => setUrl(evt.target.value)}
         />
-        <button
-          type="button"
-          onClick={() => {
-            if (
-              imageEl.current !== null &&
-              monochromeCanvasEl.current !== null
-            ) {
-              blitImage(imageEl.current, monochromeCanvasEl.current);
-            }
-          }}
-        >
-          Load image
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            if (
-              imageEl.current !== null &&
-              monochromeCanvasEl.current !== null
-            ) {
-              downloadQimz(imageEl.current, monochromeCanvasEl.current);
-            }
-          }}
-        >
-          Download Quokka image
-        </button>
+        <input type="submit" value="Add URL" />
       </form>
-    </div>
-  );
+    </Container>
+  ));
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// These must be the first lines in src/index.js
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/stable";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
